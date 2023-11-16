4809fa197024ab80e8b848764a268dc6d3cd441f
Bourqu <dybourqu@gmail.com>
2023-11-15 21:23:18 -0500

    playing with differet API methds

---
+++ script.js
@@ -0,0 +1,54 @@
+const { promisify } = require('util');
+const fs = require('fs');
+const path = require('path');
+
+async function getDataFromCube(apiUrl, productId, coordinateValues, latestN, outputPath) {
+  try {
+    // Dynamically import 'node-fetch'
+    const fetch = await import('node-fetch');
+
+    const coordinate = coordinateValues.join('.');
+    const requestBody = [{
+      "productId": productId,
+      "coordinate": coordinate,
+      "latestN": latestN
+    }];
+
+    const response = await fetch.default(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to get data, status code ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data && Array.isArray(data) && data.length > 0) {
+      // Assuming the response is an array of data objects
+      const fileName = 'output.json'; // Change this to the desired output file name
+      const filePath = path.join(outputPath, fileName);
+
+      await promisify(fs.writeFile)(filePath, JSON.stringify(data, null, 2));
+
+      console.log(`Data saved successfully: ${filePath}`);
+    } else {
+      throw new Error('Failed to get valid data from Stats Canada API');
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// Example usage:
+const apiUrl = 'https://www150.statcan.gc.ca/t1/wds/rest/getDataFromCubePidCoordAndLatestNPeriods';
+const productId = 1410035401;
+const coordinateValues = [1,2,0, 0, 0, 0, 0, 0, 0, 0, 0];
+const latestN = 1;
+const outputPath = __dirname; // You can change this to the desired output directory
+
+getDataFromCube(apiUrl, productId, coordinateValues, latestN, outputPath);
