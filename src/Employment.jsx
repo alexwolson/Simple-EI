678b9053a0d509c764fd0f1f397d3daf91f7f417
Bourqu <dybourqu@gmail.com>
2024-08-01 15:02:43 -0400

    added all req fields for Employment component

--- src/Employment.jsx
+++ src/Employment.jsx
@@ -7,16 +7,23 @@ import { Form, useForm } from 'react-hook-form';
 const Employment= () => {
 
 
+    const {register}= useForm();
 
-    return (
+
+        return (
 
         <form action="">
 
 <h3>Employment Details</h3>
 
-<input type="text" name='employerName' placeholder='Employer Name' />
-<input type="date" name='startDate' placeholder='Start Date' />
-
+<input type="text" {...register('employerName')} placeholder='Employer Name' />
+<input type="radio" {...register('currentEmployer')} />
+<p>Start Date</p>
+<input type="date" {...register('startDate',{required:true})} placeholder='Start Date' />
+<p>End Date</p>
+<input type="date" {...register('endDate')} placeholder='End Date' />
+<p>Weekly Hours</p>
+<input type="number" {...register("weeklyHours", {required:true})}/>
 
 
 
